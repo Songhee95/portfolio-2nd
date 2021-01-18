44674bab396d53d22e66252a74333ed58261dcd8
songhee <thdgml5537@gmail.com>
2021-01-18 16:41:19 -0500

    add 'about me' section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <>
         <div className='about-container'>
             <div className='about-me'>
-                <p><strong>About me</strong></p>
+                <p><strong>About Me</strong></p>
                 <p>
                     Full Stack Developer with Engineering background to think more
                     logically and process more efficiently in creating code. Recently
@@ -25,53 +25,32 @@ function About() {
                 </p>
             </div>
 
-            <div className='skill-strength'>
-                <div className='skill-set'>
-                    <p><strong>SKILL SET</strong></p>
-                    <div>
-                        <div className="logo">
-                            <i className="devicon-bootstrap-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-jquery-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-javascript-plain devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-html5-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-react-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-nodejs-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-express-original-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-mysql-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-mongodb-plain-wordmark devicon"></i>
-                        </div>
-                        <div className="logo">
-                            <i className="devicon-heroku-original-wordmark devicon"></i>
-                        </div>
+            <div className='skill-strength row'>
+                <div className='skill-set col-sm-12 col-md-12 col-lg-6'>
+                    <p><strong>Skill Set</strong></p>
+                    <div className="icons row">
+                        <i className="devicon-bootstrap-plain-wordmark devicon col"></i>
+                        <i className="devicon-jquery-plain-wordmark devicon col"></i>
+                        <i className="devicon-javascript-plain devicon col"></i>
+                        <i className="devicon-html5-plain-wordmark devicon col"></i>
+                        <i className="devicon-react-plain-wordmark devicon col"></i>
+                        <i className="devicon-nodejs-plain-wordmark devicon col"></i>
+                        <i className="devicon-express-original-wordmark devicon col"></i>
+                        <i className="devicon-mysql-plain-wordmark devicon col"></i>
+                        <i className="devicon-mongodb-plain-wordmark devicon col"></i>
+                        <i className="devicon-heroku-original-wordmark devicon col"></i>
                     </div>
                 </div>
                 
-                <div className='workplace-strength'>
-                    <div>
-                        <p><strong>WorkPlace Strength</strong></p>
-                        <div>
-                            <strong>Great Teamwork Skill</strong> |
-                            <strong>Collaboration</strong> | <strong>Fast Learner</strong> |
-                            <strong>Problem-Solving by Analyzing</strong> |
-                            <strong>Excellent Time Management</strong> |
-                            <strong>Mind Control Under Pressure</strong>
-                        </div>
+                <div className='workplace-strength col-sm-12 col-md-12 col-lg-5'>
+                    <p><strong>Workplace Strength</strong></p>
+                    <div className="row">
+                        <strong>Great Teamwork Skill</strong> 
+                        <strong>Collaboration</strong>
+                        <strong>Fast Learner</strong>
+                        <strong>Problem-Solving by Analyzing</strong> 
+                        <strong>Excellent Time Management</strong> 
+                        <strong>Mind Control Under Pressure</strong>
                     </div>
                 </div>
             </div>
