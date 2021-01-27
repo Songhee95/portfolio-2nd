efbbc9656e6d988a6eb810bfc64e628dc3af3817
songhee <thdgml5537@gmail.com>
2021-01-26 23:26:53 -0500

    router change for deploy

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import "./App.css";
 import Home from "./components/Pages/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Home />
-        <Switch>
-          <Route
-            path="/github"
-            exact
-            component={() => {
-              window.location.href = "https://github.com/Songhee95";
-              return null;
-            }}
-          />
-          <Route
-            path="/linkedIn"
-            exact
-            component={() => {
-              window.location.href =
-                "https://www.linkedin.com/in/songhee-y-821442167/";
-              return null;
-            }}
-          />
-        </Switch>
+        <Route
+          path="/github"
+          exact
+          component={() => {
+            window.location.href = "https://github.com/Songhee95";
+            return null;
+          }}
+        />
+        <Route
+          path="/linkedIn"
+          exact
+          component={() => {
+            window.location.href =
+              "https://www.linkedin.com/in/songhee-y-821442167/";
+            return null;
+          }}
+        />
       </Router>
     </>
   );
